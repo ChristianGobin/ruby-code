74adc4644b5a476fa8eea76988d1dfbc6680bcb3
Christian G <chrisgobin94@gmail.com>
2023-08-28 10:30:21 -0400

    variable practice with conditionals

---
+++ variables.rb
@@ -0,0 +1,18 @@
+# Variables in ruby
+# snake_case
+
+person_age = "25"
+person_name = "Chris"
+
+puts ("Hello! " + person_name)
+puts ("is it true that you are " + person_age + " years old!?")
+
+# conditionals - if, elsif, else, end.
+
+if (person_age > "25")
+    puts "You're in your mid life!"
+elsif (person_age < "25")
+    puts "You're young!"
+else
+    puts "You must be 25!"
+end
